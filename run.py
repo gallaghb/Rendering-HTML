650300b4263f43fa2855c9d2cf000387d280880f
gallaghb <brian.gallagher102@gmail.com>
2020-07-01 23:00:49 +0000

    second commit

--- run.py
+++ run.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def about():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
