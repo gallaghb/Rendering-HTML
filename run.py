61343505c9f623335b79b7e7dd65b7fe3b59641f
gallaghb <brian.gallagher102@gmail.com>
2020-07-02 10:30:20 +0000

    Template Inheritance

--- run.py
+++ run.py
@@ -13,8 +13,12 @@ def about():
     return render_template("about.html")
 
 @app.route('/contact')
-def about():
+def contact():
     return render_template("contact.html")
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
